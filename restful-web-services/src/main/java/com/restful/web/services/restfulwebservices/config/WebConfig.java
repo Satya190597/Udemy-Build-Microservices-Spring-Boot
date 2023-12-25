f5c7c312877a8c8a32b34fb1a85bb9f9d720ffa3
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2023-12-25 16:09:01 +0530

    Content negotiation

---
+++ restful-web-services/src/main/java/com/restful/web/services/restfulwebservices/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.restful.web.services.restfulwebservices.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .ignoreAcceptHeader(true)
+                .parameterName("mediaType")
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml",MediaType.APPLICATION_XML)
+                .mediaType("json",MediaType.APPLICATION_JSON);
+    }
+}
